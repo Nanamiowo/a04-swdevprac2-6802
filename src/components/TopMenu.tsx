6b958a56f6b14b37ab40d8fd5421b4e49b452cd2
Nanamiowo <6833219621@student.chula.ac.th>
2026-03-12 14:53:50 +0700

    edit menu bar

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,17 +1,21 @@
 import TopMenuItem from "./TopMenuItem"
+
 export default function TopMenu() {
   return (
     <div style={{
       display: "flex",
-      justifyContent: "space-between",
+      justifyContent: "flex-end",
+      alignItems: "center",
       background: "black",
       padding: "50px",
       fontSize: "50px"
     }}>
 
-      <TopMenuItem title="Booking" pageRef="/booking/"/>
+      <div style={{marginRight:"20px"}}>
+        <TopMenuItem title="Booking" pageRef="/booking/"/>
+      </div>
 
-      <img src="/img/logo.png" className="h-75" />
+      <img src="/img/logo.png" className="h-50" />
 
     </div>
   )
